7ada76fb86f89ee510e0ad4281d8b2882e1ca57a
yinnping <ciaoing@livemail.tw>
2016-10-18 12:03:55 +0000

    Fix the error of transfer a large file

---
+++ 1/Client/Client.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    public class Client
+    {
+        public static void Main(string[] args)
+        {
+            // args[0] for IP address
+            // args[1] for Port
+            // args[2] for File name
+
+            // To creat a Server side Point
+            // Get IP and Port from args[]
+            IPAddress ServerIP = IPAddress.Parse(args[0]);
+            int ServerPort = Convert.ToInt32(args[1]);
+            IPEndPoint Server = new IPEndPoint(ServerIP, ServerPort);
+
+            // Create a Socket used to connect to Server 
+            Socket ConnectToServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ConnectToServer.Connect(Server);
+
+            string FileName = args[2];
+
+            // Send File name to Server            
+            ConnectToServer.Send(Encoding.ASCII.GetBytes(args[2]));
+
+            byte[] SureSize = new byte[1024];               // Used to store file size
+
+            // Determice the length actually have data
+            int tmp = ConnectToServer.Receive(SureSize);    
+
+            // Findout the file size
+            int DataSize = Convert.ToInt32(Encoding.ASCII.GetString(SureSize, 0, tmp));
+
+            byte[] buffer = new byte[4096];
+            
+            // Creat a FileStream to store file
+            Stream file = new FileStream(FileName, FileMode.Append);
+            int UnGetSize = DataSize;       // Remind data
+            
+            while (UnGetSize > 0)
+            {                
+                UnGetSize -= ConnectToServer.Receive(buffer);           
+                file.Write(buffer, 0, buffer.Length);                
+            }
+            
+            Console.WriteLine("Receive Sucessful!");
+        }
+    }
+}
---
+++ 1/Server/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Server
+    {
+        public static void Main(string[] args)
+        {
+            // args[0] for IP address
+            // args[1] for Port
+            
+            IPAddress ListenIP = IPAddress.Parse(args[0]);
+            int ListenPort = Convert.ToInt32(args[1]);                
+            
+            // Create an end point with IP and Port 
+            IPEndPoint ListenOn = new IPEndPoint(ListenIP, ListenPort);
+
+            // Create a socket to handle connection
+            Socket ConnectFromClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            ConnectFromClient.Bind(ListenOn);   // Bind Socket on end point which is listening
+            ConnectFromClient.Listen(10);       // Only allow 10 connections
+
+            byte[] buffer = new byte[1024];     // Use to receive the file name from client
+            
+            while(true)
+            {
+                Console.WriteLine("Wait for Client ... ");
+                Socket Client = ConnectFromClient.Accept();     // Handle connection
+                
+                int len = Client.Receive(buffer);               // Get the data size (byte)
+
+                // Decode the receive data from ASCII to string
+                string FileName = Encoding.ASCII.GetString(buffer, 0, len);
+                FileInfo FileCheck = new FileInfo(FileName);
+
+                // Send file Size to Client
+                Client.Send(Encoding.ASCII.GetBytes(FileCheck.Length.ToString()));                
+                
+                // Send File to Client 
+                byte[] data = new byte[FileCheck.Length];
+                File.ReadAllBytes(FileName).CopyTo(data, 0);             
+                Client.Send(data);
+
+                Console.WriteLine("Send Sucessful!");                    
+                Console.WriteLine();
+            }                    
+        }
+    }
+}

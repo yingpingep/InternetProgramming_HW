f48fe4c7e46e8181eca8fa6c2f8bef536f5580db
yinnping <ciaoing@livemail.tw>
2016-11-16 23:22:58 +0800

    Finish Three-way handshack

--- 2/Client/Client.cs
+++ 2/Client/Client.cs
@@ -14,14 +14,38 @@ namespace Client
     class Client
     {
         static void Main(string[] args)
-        {
+        {            
             IPEndPoint serverIP = new IPEndPoint(IPAddress.Parse(args[0]), Convert.ToInt32(args[1]));
             Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            PacketFormate test = new PacketFormate(true, false, false, 1, 1, 3456789);
-            byte[] buffer = Method.PacketToByteArray(test);
 
-            udpSocket.SendTo(buffer, serverIP);
-            Console.WriteLine("Sent");
-        }     
+            Random random = new Random();
+            int startNumber = random.Next() % 200;    
+            byte[] buffer = new byte[1024];
+            bool connected  = Handshake(udpSocket, serverIP, ref startNumber,buffer);
+
+            // TODO Receive
+        }
+
+        static bool Handshake(Socket udpSocket, IPEndPoint serverIP, ref int startNumber, byte[] buffer)
+        {
+            PacketFormate packet = new PacketFormate(true, false, false, startNumber++, 0, 0);
+            buffer = Method.PacketToByteArray(packet);
+            udpSocket.SendTo(buffer, serverIP);            
+
+            Array.Clear(buffer, 0, buffer.Length);
+            EndPoint remoteEP = serverIP;
+            int len = udpSocket.ReceiveFrom(buffer, ref remoteEP);   
+            packet = Method.ByteArrayToPacket(buffer, len);        
+
+            if (packet.ackNumber == startNumber)
+            {
+                packet = new PacketFormate(false, true, false, startNumber++, packet.sequenceNumber + 1, 0);
+                Array.Clear(buffer, 0, buffer.Length);
+                buffer = Method.PacketToByteArray(packet);
+                udpSocket.SendTo(buffer, serverIP);                
+            }
+
+            return true;
+        }
     }
 }
--- 2/Server/Server.cs
+++ 2/Server/Server.cs
@@ -23,12 +23,35 @@ namespace Server
             udpSocket.Bind(listenOn);
 
             byte[] buffer = new byte[1024];
+            Random random = new Random();
+            int startNumber = random.Next() % 200;
+            EndPoint remoteEP = Handshake(udpSocket, ref startNumber, buffer);
 
+            // TODO Transfer
+        }
+        
+        static EndPoint Handshake(Socket udpSocket, ref int startNumber, byte[] buffer)
+        {
             EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
-            int revlen = udpSocket.ReceiveFrom(buffer, ref remoteEP);
-            PacketFormate tt = Method.ByteArrayToPacket(buffer, revlen);
 
-            Console.WriteLine(tt.data + "   " + revlen);
-        }   
+            int len = udpSocket.ReceiveFrom(buffer, ref remoteEP);
+            PacketFormate packet = Method.ByteArrayToPacket(buffer, len);
+            Console.WriteLine("Start three-way handshake");           
+            
+            if (packet.syn)
+            {                
+                Array.Clear(buffer, 0, buffer.Length);
+                packet = new PacketFormate(false, true, false, startNumber++, packet.sequenceNumber + 1, 0);            
+                buffer = Method.PacketToByteArray(packet);
+                udpSocket.SendTo(buffer, remoteEP);                
+            }
+
+            Array.Clear(buffer, 0, buffer.Length);
+            len = udpSocket.ReceiveFrom(buffer, ref remoteEP);            
+            packet = Method.ByteArrayToPacket(buffer, len);
+
+            // if (packet.ackNumber == ++startNumber)           
+            return remoteEP;
+        }
     }
 }
